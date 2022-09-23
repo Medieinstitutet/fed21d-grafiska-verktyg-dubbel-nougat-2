3e3b0a9febfab65dbdb73132de70f1b057f59725
joelhagg <joel.pettersson90@gmail.com>
2022-09-23 06:51:49 +0200

    add base code

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log("hej");
+
+let aceptAllCookiesBtn = document.getElementById("aceptAllCookiesBtn");
+let acceptnecessaryCookiesBtn = document.getElementById(
+  "acceptnecessaryCookiesBtn"
+);
+let menuIcon = document.getElementById("menuIcon");
+let menuWraper = document.getElementById("menuWraper");
+
+let menuOpen = true;
+
+aceptAllCookiesBtn.addEventListener("click", () => {
+  document.getElementById("cookies").style.display = "none";
+});
+
+acceptnecessaryCookiesBtn.addEventListener("click", () => {
+  document.getElementById("cookies").style.display = "none";
+});
+
+menuIcon.addEventListener("click", () => {
+  console.log("click");
+  if (menuOpen == false) {
+    menuOpen = true;
+    console.log("true");
+    menuWraper.style.display = "none";
+  } else {
+    menuWraper.style.display = "flex";
+    menuOpen = false;
+    console.log("false");
+  }
+  console.log(menuOpen);
+});
